8791e9b182e95b4040d08ca87642cec4e35a4869
Andreas Richardson <andreas.richardson@epfl.ch>
2020-05-18 14:24:14 +0100

    add PDFs for histograms w/ skew-normal fit

--- analysis/fits.m
+++ analysis/fits.m
@@ -11,20 +11,32 @@ opts = setvartype(opts,{'Q11','Q13'},'categorical') ;
 
 d = readtable(file,opts);
 
-%% histogram
+%% histogram + fit
 
-figure
-hold on
-histogram(d.Q2_1,'Normalization','probability')
-boxplot(d.Q2_1,'Positions',0.19,'Orientation','Horizontal','Colors','g','Widths',0.01)
-ax = gca;
-ax.YAxis.TickValues = 0:0.05:0.2;
-ax.YAxis.TickLabelsMode = 'auto';
-ax.YAxis.Limits = [0 0.2];
+for q = {'Q2_1' 'Q4_1' 'Q4_2'}
+% for q = {'x1_Q7_1'}
+    q = char(q);
 
-% fit skew normal
-fit.Q2_1 = fitdist(d.Q2_1,'EpsilonSkewNormal');
+    figure
+    hold on
+    histogram(d.(q),'Normalization','probability')
 
-sampleRes.Q2_1 = 0:0.2:15;
-fitVals.Q2_1 = pdf(fit.Q2_1,sampleRes.Q2_1);
-plot(sampleRes.Q2_1,fitVals.Q2_1,'LineWidth',2)
+    % fit skew normal
+    fit.(q) = fitdist(d.(q),'EpsilonSkewNormal');
+
+    % plot skew normal
+    sampleRes.(q) = 0:24;
+    fitVals.(q) = pdf(fit.(q),sampleRes.(q));
+    plot(sampleRes.(q),fitVals.(q),'LineWidth',2)
+    
+    title(q,'Interpreter','none')
+    
+    figExport(12,8,['fit-' q])
+
+%     % add boxplot for comparison
+%     boxplot(d.(q),'Positions',0.19,'Orientation','Horizontal','Colors','g','Widths',0.01)
+%     ax = gca;
+%     ax.YAxis.TickValues = 0:0.05:0.2;
+%     ax.YAxis.TickLabelsMode = 'auto';
+%     ax.YAxis.Limits = [0 0.2];
+end
